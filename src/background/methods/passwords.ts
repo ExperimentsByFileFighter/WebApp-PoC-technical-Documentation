62b186dd9727eaacbf1a54e4f05b0e4a423cb320
Gimleux <56401687+Gimleux@users.noreply.github.com>
2022-03-02 13:39:59 +0100

    added examples to doc

--- src/background/methods/passwords.ts
+++ src/background/methods/passwords.ts
@@ -8,6 +8,10 @@ const salt = "FileFighterWithSomeSalt"
  * It hashes the password using the SHA-256 algorithm.
  * @param password - The password to hash.
  * @returns {string} The hash of the password.
+ * @example
+ * // returns 7B1A454E41944B1FD6EB9AC7B1CA6998C4D9263650D257B9B54D0A8FA8A2EFD5
+ * let hashedPassword = await hashPassword("Here1AmHere1Remain.");
+ *
  */
 async function hashPassword(password: string) {
     if (!crypto.subtle) {
